00ece6b9c7b45e98832a947887a3517696967e3d
dannyb21892 <dannyb21892@verizon.net>
2018-03-11 15:39:39 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now.to_s.split(" ")[1][0..1].to_i
+    if time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+end
